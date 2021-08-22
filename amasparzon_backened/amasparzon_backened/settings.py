82142221c1613cf22d99f5957ab6722fad4b9251
markusgerman <german@myconsult.de>
2021-08-22 20:21:02 +0200

    Update settings.py

--- amasparzon_backened/amasparzon_backened/settings.py
+++ amasparzon_backened/amasparzon_backened/settings.py
@@ -4,12 +4,15 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = '2x$e%!k_u_0*gq0s4!_u(2(^lpy&gir0hg)q&5nurj0-sseuav'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+if DEBUG == False:
+    SECRET_KEY = config("SECRET_KEY")
+else:
+    SECRET_KEY = '2x$e%!k_u_0*gq0s4!_u(2(^lpy&gir0hg)q&5nurj0-sseuav'
 
 ALLOWED_HOSTS = []
 
