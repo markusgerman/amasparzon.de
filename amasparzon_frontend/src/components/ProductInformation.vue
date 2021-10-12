fe28e8b5464fd135033c3ed362692d2eb38c06a2
markusgerman <german@myconsult.de>
2021-10-12 23:35:10 +0200

    Productview

--- amasparzon_frontend/src/components/ProductInformation.vue
+++ amasparzon_frontend/src/components/ProductInformation.vue
@@ -1,40 +1,61 @@
 <template>
-    <div class="product-view">
-        <label for="amazon-link">Hier Amazon Link einfügen</label>
-        <input type="url" id="amazonlink" name="amazonlink" v-model="amazonlink">
-        
-        <div class="product">
-            <div class="product-name"></div>
-            <div class="product-image"></div>
-            <div class="product-information"></div>
-        </div>
+  <div class="product-view">
+    <label for="amazon-link">Hier Amazon Link einfügen</label>
+    <input
+      type="url"
+      id="amazonlink"
+      name="amazonlink"
+      @paste="onPaste"
+      v-model="amazonlink"
+    />
 
-        <label for="email">E-Mail-Adresse</label>
-        <input type="email" id="email" name="email" v-model="email">
+    <div v-if="productimage">
+      <div class="product">
+        <div class="product-name" id="productname">{{ productname }}</div>
+        <img class="product-image" :src="productimage" alt="productimage" />
+        <div class="product-price" id="productprice">{{ productprice }}</div>
+      </div>
+
+        
+      <label for="email">E-Mail-Adresse</label>
+      <input type="email" id="email" name="email" v-model="email" />
+      <p> Sie werden benachrichtigt sobald sich der Preis ändert </p>
     </div>
+    
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'ProductInformation',
+import axios from "axios";
 
-    data() {
-        return {
-            amazonlink : "",
-            email : ""
-        }
+export default {
+  name: "ProductInformation",
+  data() {
+    return {
+      amazonlink: "",
+      email: "",
+      productname: "",
+      productimage: "",
+      productprice: "",
+    };
+  },
+  methods: {
+    onPaste(evt) {
+      this.GetAmazonProductAsync(
+        evt.clipboardData.getData("text").replace("https://www.amazon.com/", "")
+      );
     },
 
-    methods : {
-        getAmazonProduct(){
-            return this.amazonlink
-        }
-    }
-}
-
-
+    async GetAmazonProductAsync(product_resource) {
+      await axios.get(`api/products/${product_resource}`).then((response) => {
+        this.productname = response.data.name;
+        this.productimage = response.data.image;
+        this.productprice = response.data.price;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
