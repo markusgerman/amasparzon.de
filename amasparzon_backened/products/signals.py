fbca7cc6b3905d6134d6eac723f61b961a0390a1
markusgerman <german@myconsult.de>
2021-12-07 00:43:54 +0100

    #9

--- amasparzon_backened/products/signals.py
+++ amasparzon_backened/products/signals.py
@@ -3,16 +3,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import get_template
 
-#custom user model
-from django.contrib.auth import get_user_model
-Users = get_user_model()
-
 from amasparzon_backened.settings import EMAIL_HOST_USER
 
-@receiver(post_save, sender=Users)
+from .models import Product
+
+@receiver(post_save, sender=Product)
 def create_user_profile(sender, instance, created, **kwargs):
     """
-    Send email confirmation to user when account is created
+    Send email confirmation to user when product is created
     """
     if created:
 
@@ -21,7 +19,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         message = get_template('account_created_mail.html').render(context)
 
         mail = EmailMessage(
-            subject =  "Vielen Dank für deine Anfrage!",
+            subject =  "",
             body = message,
             from_email = EMAIL_HOST_USER,
             to = [instance.email],
@@ -29,4 +27,3 @@ def create_user_profile(sender, instance, created, **kwargs):
         mail.content_subtype = "html"
 
         return mail.send()
-
